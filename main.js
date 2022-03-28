66a15d1211469cf4e58779f842ed8183e964204f
Shalitha Suranga <shalithasuranga@gmail.com>
2022-03-29 04:27:55 +0530

    Add initial project files

---
+++ main.js
@@ -0,0 +1,52 @@
+const fastify = require('fastify')();
+const path = require('path')
+
+fastify.register(require('fastify-websocket'), {options: {clientTracking: true}});
+fastify.register(require('fastify-static'), {
+    root: path.join(__dirname, 'www')
+})
+
+fastify.addHook('preValidation', async (request, reply) => {
+    if(request.routerPath == '/chat' && !request.query.username) {
+        reply.code(401).send('Connection rejected');
+    }
+})
+
+fastify.get('/chat', { websocket: true }, (connection, req) => {
+    // New user
+    broadcast({
+        sender: '__server',
+        message: `${req.query.username} joined`
+    });
+
+    // Leaving user
+    connection.socket.on('close', () => {
+        broadcast({
+            sender: '__server',
+            message: `${req.query.username} left`
+        });
+    });
+
+    // Broadcast incoming message
+    connection.socket.on('message', (message) => {
+        message = JSON.parse(message.toString());
+        broadcast({
+            sender: req.query.username,
+            ...message
+        });
+    });
+});
+
+fastify.listen({ port: 3000 }, (err, address) => {
+    if(err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at: ${address}`);
+});
+
+function broadcast(message) {
+    for(let client of fastify.websocketServer.clients) {
+        client.send(JSON.stringify(message));
+    }
+}
